995fc6b518bba42bd73ed20740ed0c6d15586078
MattLeuterio <leuterio.matteo@gmail.com>
2020-10-05 09:13:55 +0200

    Skill&Project section fixed

--- src/shared/containerVariants.jsx
+++ src/shared/containerVariants.jsx
@@ -1,14 +1,17 @@
  const containerVariants = {
     hidden: { 
       scale: 0,
+      overflow: "hidden"
     },
     visible: {
       scale: 1,
       transition: { duration: .7},
+      overflow: "visible"
     },
     exit: {
       scale: 10,
       opacity: 0,
+      overflow: "hidden",
       transition: { duration: .7, ease: 'easeIn' }
     }
   }
