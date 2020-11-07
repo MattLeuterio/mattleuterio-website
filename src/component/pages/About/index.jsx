fb2fa620c597e8a8b48bc8d72922499c57397d3d
Matteo Francesco Leuterio <leuterio.matteo@gmail.com>
2020-11-07 22:14:37 +0100

    [MOB] new Menu icon - clean code

--- src/component/pages/About/index.jsx
+++ src/component/pages/About/index.jsx
@@ -1,43 +1,46 @@
-import React from 'react';
-import classes from './styles.module.scss';
-import { motion } from 'framer-motion';
-import Layout from '../../../shared/hoc/Layout';
+import React from "react";
+import classes from "./styles.module.scss";
+import { motion } from "framer-motion";
+import Layout from "../../../shared/hoc/Layout";
 
-import containerVariants from '../../../shared/containerVariants';
+import containerVariants from "../../../shared/containerVariants";
 
 const about = () => {
-
   return (
     <Layout>
-      <motion.div className={classes.About}
+      <motion.div
+        className={classes.About}
         variants={containerVariants}
         initial="hidden"
         animate="visible"
         exit="exit"
       >
-
         <section className={classes.Hello}>
           <div className={classes[`Hello--text`]}>
             <h1>Hello, I'm Matt.</h1>
           </div>
           <div className={classes[`Hello--img-container`]}>
-            <img src="./img/matt-leuterio.png" alt="Matt Leuterio"/>
+            <img src="./img/matt-leuterio.png" alt="Matt Leuterio" />
           </div>
         </section>
 
         <section className={classes.Summary}>
+          <p>Ambizioso e curioso, amante della tecnologia e del codice.</p>
           <p>
-            Lorem, ipsum dolor sit amet consectetur adipisicing elit. Voluptatum dolorum vel, porro itaque pariatur atque distinctio! Eaque saepe possimus neque, aspernatur tenetur laboriosam dolore consequuntur accusamus, quia ratione labore magnam eligendi? Debitis magnam repellendus porro similique et, amet nesciunt quis itaque sit corrupti sequi incidunt in iure harum doloremque, dicta temporibus. Exercitationem delectus laborum laboriosam voluptates dolorum ex perferendis voluptatibus amet iste temporibus suscipit quis tempora officiis nobis, qui quasi harum, eum ducimus, officia vel quibusdam dolore accusantium. Facere autem quod esse quibusdam a doloremque repudiandae qui perferendis minima maxime necessitatibus, at, et nulla commodi repellendus rerum eligendi earum quas numquam modi! Ipsa iusto, dolores minima a maiores quibusdam et nihil exercitationem omnis accusantium!
+            Amo vedere le mie creazioni prendere forma e discuterne con le
+            persone per migliorarne ogni dettaglio. Tutto ciò ha contribuito a
+            far nascere in me la passione per il front-end e la user experience.
           </p>
+          <p>
+            Ho lavorato come Videomaker cercando ogni giorno di migliorarmi sia
+            come persona che come professionista ed affinando le mie skills sono
+            arrivato a lavorare alla produzione di un documentario a New York.
+          </p>
+          <p>Adoro le sfide ed il codice pulito ed efficiente.</p>
         </section>
-
-
-        
-        
       </motion.div>
     </Layout>
-  )
-}
-
+  );
+};
 
 export default about;
--- src/component/pages/Projects/SingleProject/index.jsx
+++ src/component/pages/Projects/SingleProject/index.jsx
@@ -1,38 +1,33 @@
-import React from 'react';
+import React from "react";
 import { useParams } from "react-router";
-import classes from './styles.module.scss';
-import { motion } from 'framer-motion';
-import Layout from '../../../../shared/hoc/Layout';
+import classes from "./styles.module.scss";
+import { motion } from "framer-motion";
+import Layout from "../../../../shared/hoc/Layout";
 
-
-import containerVariants from '../../../../shared/containerVariants';
-import listProjs from '../../../../data.json';
-
-
+import containerVariants from "../../../../shared/containerVariants";
+import listProjs from "../../../../data.json";
 
 const SingleProject = () => {
-  const {id} = useParams();
+  const { id } = useParams();
 
   let projs = "";
-  
-  if (document.location.search === "?f") {
-    projs = listProjs.projects.film;
-  }  else {
-    projs = listProjs.projects.dev;
-  }
-  
+  document.location.search === "?f"
+    ? (projs = listProjs.projects.film)
+    : (projs = listProjs.projects.dev);
+
   return (
     <Layout>
-      <motion.div className={classes.SingleProject}
+      <motion.div
+        className={classes.SingleProject}
         variants={containerVariants}
         initial="hidden"
         animate="visible"
         exit="exit"
       >
-      {
-        projs.map((dev) => {
-          if(dev.id == id) {
-            return (
+        {projs.map(
+          (dev) =>
+            // eslint-disable-next-line
+            dev.id == id && (
               <React.Fragment key={dev.id}>
                 <div className={classes[`SingleProject-title`]}>
                   <h1>{dev.title}</h1>
@@ -44,48 +39,51 @@ const SingleProject = () => {
                   <div className={classes[`SingleProject-info--tech`]}>
                     <h3>Tech</h3>
                     <ul>
-                      {
-                        dev.tech.map( (tech, index) => {
-                          return <li key={index}>{tech}</li>
-                        })
-                      }
+                      {dev.tech.map((tech, index) => {
+                        return <li key={index}>{tech}</li>;
+                      })}
                     </ul>
                   </div>
                   <div className={classes[`SingleProject-info--content`]}>
-                    <div className={classes[`SingleProject-info--content-description`]}>
+                    <div
+                      className={
+                        classes[`SingleProject-info--content-description`]
+                      }
+                    >
                       <h3>Description</h3>
                       <p>{dev.content}</p>
                     </div>
-                    <div className={classes[`SingleProject-info--content-links`]}>
+                    <div
+                      className={classes[`SingleProject-info--content-links`]}
+                    >
                       <h3>Links</h3>
-                      <div className={classes[`SingleProject-info--content-links--list`]}>
-                        {
-                          dev.links.map( (link, index) => {
-                          return  <motion.a 
-                                    key={index} 
-                                    href={link[1]} 
-                                    target="_blank"
-                                    whileHover={{
-                                      backgroundColor: "#B740F0"
-                                    }}
-                                    >{link[0]}</motion.a>
-                          })
+                      <div
+                        className={
+                          classes[`SingleProject-info--content-links--list`]
                         }
+                      >
+                        {dev.links.map((link, index) => (
+                          <motion.a
+                            key={index}
+                            href={link[1]}
+                            target="_blank"
+                            whileHover={{
+                              backgroundColor: "#B740F0",
+                            }}
+                          >
+                            {link[0]}
+                          </motion.a>
+                        ))}
                       </div>
                     </div>
                   </div>
                 </div>
-              </React.Fragment >
-  
-              )
-          }
-          })
-        }
+              </React.Fragment>
+            )
+        )}
       </motion.div>
     </Layout>
-  )
-}
-
-
+  );
+};
 
 export default SingleProject;
